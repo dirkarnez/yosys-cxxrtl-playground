35cb3498fcebead653277651ec17788ad28eec39
Dirk Arnez <smalldirkalex@gmail.com>
2025-09-23 02:02:24 +0800

    - update project/compile.sh

--- project/compile.sh
+++ project/compile.sh
@@ -2,4 +2,4 @@
 # -header
 yosys -p "read_verilog src/chip.v; write_cxxrtl src/chip_cxxrtl_generated.h" || exit 1
 source /opt/emsdk/emsdk_env.sh
-emcc -O3 -std=c++14 -I `yosys-config --datdir`/include -Isrc main.cpp --no-entry -sERROR_ON_UNDEFINED_SYMBOLS=0 -sINITIAL_MEMORY=128kb -sALLOW_MEMORY_GROWTH -sTOTAL_STACK=64kb -o chip.wasm
+emcc -O3 -std=c++14 -I `yosys-config --datdir`/include -lembind -Isrc main.cpp --no-entry -sERROR_ON_UNDEFINED_SYMBOLS=0 -sINITIAL_MEMORY=128kb -sALLOW_MEMORY_GROWTH -sTOTAL_STACK=64kb -o chip.js
