e5c51ed3445c84f49d37ac115c25e672647eb357
dirkarnez <smalldirkalex@gmail.com>
2026-02-15 13:46:49 +0000

    - wip

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
+#include <fstream>
 #include <emscripten/bind.h>
 #include <backends/cxxrtl/runtime/cxxrtl/cxxrtl_vcd.h>  // oss-cad-suite\share\yosys\include\backends\cxxrtl\runtime\cxxrtl
 #include "chip_cxxrtl_generated.h"
 
 using namespace emscripten;
+using namespace std;
 
 void chip_init(void) {
-        cxxrtl_design::p_chip design;
-        design.step();
-        // clk
-        // while (1) {
-        //   design.p_clk.set(false);
-        //   design.step();
-        //   design.p_clk.set(true);
-        //   design.step();
-        // }
+    // vcd_writer is the CXXRTL object that's responsible of creating a string with
+    // the VCD file contents.
+    cxxrtl::vcd_writer vcd;
+    vcd.timescale(1, "us");
+    
+    cxxrtl_design::p_chip design;
+    design.step();
+    // clk
+    // while (1) {
+    //   design.p_clk.set(false);
+    //   design.step();
+    //   design.p_clk.set(true);
+    //   design.step();
+    // }
 }
 
 EMSCRIPTEN_BINDINGS(my_module) {
     function("chip_init", &chip_init);
-    // constant("FNM_PATHNAME", FNM_PATHNAME);
-    // constant("FNM_NOESCAPE", FNM_NOESCAPE);
-    // constant("FNM_PERIOD", FNM_PERIOD);
-    
-    // constant("FNM_NOMATCH", FNM_NOMATCH);
-    
-    // constant("FNM_FILE_NAME", FNM_FILE_NAME);
-    // constant("FNM_LEADING_DIR", FNM_LEADING_DIR);
-    // constant("FNM_CASEFOLD", FNM_CASEFOLD);
-    // constant("FNM_EXTMATCH", FNM_EXTMATCH);
 }
